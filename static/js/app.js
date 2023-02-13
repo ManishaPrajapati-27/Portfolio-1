919666270ef8e5a8b6f13349174924e58ae082db
Manisha <manprajapati268@gmail.com>
2023-02-14 00:26:37 +0530

    first portfolio

---
+++ static/js/app.js
@@ -0,0 +1,20 @@
+AOS.init({
+  duration: 1500,
+});
+
+// Sidebar Toggle
+
+// const sidebarToggle = () => {
+//   // Variables
+//   const toggle = document.querySelector(".toggle");
+//   const dashboard = document.querySelector(".js-practice-sidebar");
+
+//   // Function
+//   const clickEvent = () => {
+//     dashboard.classList.toggle("sb-active");
+//   };
+
+//   // Events
+//   toggle.addEventListener("click", clickEvent);
+// };
+// sidebarToggle();
